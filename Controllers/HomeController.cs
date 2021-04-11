7ec0ca914f9a3473756a0b6c34f461c19f1381e4
unknown <ruchirahindlekar1001@gmail.com>
2021-04-11 21:16:24 +0530

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Act.Models;
+using Microsoft.AspNet.Identity;
+
+namespace Act.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+        public JsonResult GetEvents()
+        {
+            using (CalEntities dc = new CalEntities())
+            {
+                string UID = User.Identity.GetUserId(); ;
+                var ToDo = dc.Events.Where(a => a.ID == UID).ToList();
+                return new JsonResult { Data = ToDo, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+        [HttpPost]
+        public JsonResult SaveEvent(Event e)
+        {
+            var status = false;
+            using (CalEntities dc = new CalEntities())
+            {
+               
+                if (e.EventID > 0)
+                {
+
+                    //Update the event
+                    var v = dc.Events.Where(a => a.EventID == e.EventID).FirstOrDefault();
+                    if (v != null)
+                    {
+                        
+                        v.Subject = e.Subject;
+                        v.Start = e.Start;
+                        v.End = e.End;
+                        v.Description = e.Description;
+                        v.IsFullDay = e.IsFullDay;
+                        v.ThemeColor = e.ThemeColor;
+                       
+                    }
+                }
+                else
+                {
+                    string UID = User.Identity.GetUserId();
+                    e.ID = UID;
+                    dc.Events.Add(e);
+                }
+                dc.SaveChanges();
+                status = true;
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+        [HttpPost]
+        public JsonResult DeleteEvent(int eventID)
+        {
+            var status = false;
+            using (CalEntities dc = new CalEntities())
+            {
+                var v = dc.Events.Where(a => a.EventID == eventID).FirstOrDefault();
+                if (v != null)
+                {
+                    dc.Events.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
+
+    }
+
+}
